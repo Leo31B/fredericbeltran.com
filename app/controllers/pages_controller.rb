86002d2b545b9b8992bc1554f4b7f6f11e87adb0
Luc Mosser <luc.mosser86@orange.fr>
2019-12-20 11:43:39 +0100

    routing done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  def preparation_mentale
+  end
 
-  def home
+  def facteur_humain
+  end
+
+  def formations
+  end
+
+  def livres_docs
+  end
+
+  def blog
+  end
+
+  def a_propos
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'a_propos', to: 'pages#a_propos'
+  get 'blog', to: 'pages#blog'
+  get 'livres_docs', to: 'pages#livres_docs'
+  get 'formations', to: 'pages#formations'
+  get 'facteur_humain', to: 'pages#facteur_humain'
+  get 'preparation_mentale', to: 'pages#preparation_mentale'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
