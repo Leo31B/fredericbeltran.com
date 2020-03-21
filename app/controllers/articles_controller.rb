94790a1cff816d72e30244eb8f8fb26fe4a4f2c9
l b <poumtak@yahoo.fr>
2020-03-21 23:05:22 +0100

    save2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @article = Article.all
-    # @articles = Article.paginate(page: params[:page], per_page: 4)
+    @articles = Article.paginate(page: params[:page], per_page: 4)
   end
 
   def create
